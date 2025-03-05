1927337968e6b03c9a4b8f3b44fcc4e12fc5b2bc
AbhishekKumarSah99 <abhishekkumar220801@acropolis.in>
2025-03-05 17:17:27 +0530

    again frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,31 +1,11 @@
-import React, { createContext, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
-export const Context = createContext({
-  isAuthorized: false,
-});
-
-const AppWrapper = () => {
-  const [isAuthorized, setIsAuthorized] = useState(false);
-  const [user, setUser] = useState({});
-
-  return (
-    <Context.Provider
-      value={{
-        isAuthorized,
-        setIsAuthorized,
-        user,
-        setUser,
-      }}
-    >
-      <App />
-    </Context.Provider>
-  );
-};
+import { Provider } from "react-redux";
+import store from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AppWrapper />
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
